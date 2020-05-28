89833c4b0752ecd2103987097cd0e81bc9005a9f
taruj khan <tarujkhan@DESKTOP-E8JDKB5.localdomain>
2020-05-28 09:53:24 -0500

    Complete

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
+require_relative 'config/environment'
 class App < Sinatra::Base
-
-	get '/' do
-		erb :index
-	end
-
-
-end
+	
+	 get '/hello' do
+		erb :hello
+	 end
+   
+	 get '/goodbye' do 
+	   erb :goodbye 
+   end
+   
+	 get '/date' do 
+	   erb :date 
+	 end 
+   end 
